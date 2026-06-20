9a160061c298e345c1b3f20f932ea08010a87002
Thomas Bocalon <tchongass1@gmail.com>
2026-06-20 01:31:50 -0300

    pages delete

--- main/preload-popup.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('pluginPopupAPI', {
-  getContext: () => ipcRenderer.invoke('plugins-popup-get-context'),
-  callHost: (method, ...args) => ipcRenderer.invoke('plugins-popup-rpc-request', { method, args })
-});
