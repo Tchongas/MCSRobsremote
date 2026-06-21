6368aa05eaba0d174759594f35098444f0853191
Thomas Bocalon <tchongass1@gmail.com>
2026-06-21 16:04:38 -0300

    exmaple

--- docs/components.js
+++ docs/components.js
@@ -34,9 +34,9 @@
           children: [
             { id: 'plugins-documentation', label: 'Documentation', href: docsDir + 'plugins/documentation.html' },
             { id: 'plugins-example-dashboard', label: 'Example: Dashboard', href: docsDir + 'plugins/example-dashboard.html' },
-            { id: 'plugins-example-create', label: 'Example: Create', href: docsDir + 'plugins/example-create.html' },
-            { id: 'plugins-example-automation', label: 'Example: Buttons', href: docsDir + 'plugins/example-automation.html' },
-            { id: 'plugins-example-workspace', label: 'Example: Workspace', href: docsDir + 'plugins/example-workspace.html' },
+            //{ id: 'plugins-example-create', label: 'Example: Create', href: docsDir + 'plugins/example-create.html' },
+            //{ id: 'plugins-example-automation', label: 'Example: Buttons', href: docsDir + 'plugins/example-automation.html' },
+            //{ id: 'plugins-example-workspace', label: 'Example: Workspace', href: docsDir + 'plugins/example-workspace.html' },
           ],
         },
       ],
